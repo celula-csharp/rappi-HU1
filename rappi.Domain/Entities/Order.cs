0a2bcf977dbaef43b21895d5c8da08d3b9054f38
Edison Alvarez <edisonalvarez32@gmail.com>
2025-10-24 10:53:43 -0500

    Remove sensitive config file from tracking

---
+++ rappi.Domain/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace rappi.Domain.Entities;
+
+public class Order
+{
+    public int Id { get; set; }
+    
+    //configuracion de la FK
+    [ForeignKey("Customer")]
+    public int CustomerId { get; set; } // fk
+    public virtual Customer customer { get; set; }  //
+    
+    
+    public DateTime OrderDate { get; set; }
+    public string Status { get; set; }
+}
